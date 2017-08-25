4f2e487c6a284cf88d6a85cde5528b99ec290fb8
Git-Admin <eric.leihe@gmail.com>
2017-08-24 21:16:33 -0700

    fix reset_password bug

--- test/integration/Users.js
+++ test/integration/Users.js
@@ -55,7 +55,7 @@ describe('User', () => {
   describe('Activation', () => {
     var veriCode;
     var activationToken;
-    var newPassword = 'Abcd1234';
+
 
     it("Step1. Get Verification Code", (done) => {
       var data = {
@@ -121,8 +121,13 @@ describe('User', () => {
           done(err);
         });
     });
+  });
 
-    it('Reset Password', (done) => {
+  describe('Reset password', () => {
+
+
+    it('By user self service', (done) => {
+      var newPassword = 'Abcd1234';
       return request(app)
         .post('/identity/oauth2/token')
         .set('X-Authub-Account', masterAccount.name)
@@ -140,6 +145,51 @@ describe('User', () => {
             .set('Authorization', "Bearer " + token.access_token)
             .send({
                 old_password: testUser1.password,
+                new_password: newPassword,
+            })
+            .expect(200)
+        })
+        .then((res) => {
+          return request(app)
+            .post('/identity/oauth2/token')
+            .set('X-Authub-Account', masterAccount.name)
+            .send({
+                username: testUser1.username,
+                password: newPassword,
+                grant_type: 'password'
+            })
+            .expect(200)
+        })
+        .then((res) => {
+          done();
+        })
+        .catch((err) => {
+          done(err);
+        })
+    })
+
+
+    it('By Administrator', (done) => {
+      var newPassword = 'Abcd123456';
+
+      return request(app)
+        .post('/identity/oauth2admin/token')
+        .set('X-Authub-Account', masterAccount.name)
+        .send({
+          username: masterAccount.username,
+          password: masterAccount.password,
+          grant_type: 'password'
+        })
+        .expect(200)
+        .then((res) => {
+          let token = res.body;
+          return request(app)
+            .post('/identity/users/reset_password')
+            .set('X-Authub-Account', masterAccount.name)
+            .set('Authorization', "Bearer " + token.access_token)
+            .send({
+                username: testUser1.username,
+                old_password: "",
                 new_password: newPassword,
             })
             .expect(200)
@@ -180,40 +230,40 @@ describe('User', () => {
         })
     }).timeout(5000);
 
-    it('Change password with forgot password token', (done) => {
-      dbs.connectToMaster((err, db) => {
-        db.model('ForgotPasswordRequest')
-          .findOne({ identity: testUser1.email, used: false })
-          .sort({ createdAt: 'desc'})
-          .then( forgotPasswordRequest => {
-            return request(app)
-              .post('/identity/users/forgot_password')
-              .set('X-Authub-Account', masterAccount.name)
-              .send({
-                email: forgotPasswordRequest.identity,
-                token: forgotPasswordRequest.token,
-                password: 'Abc789'
-              })
-              .expect(200)
-          })
-          .then( res => {
-            return request(app)
-              .post('/identity/oauth2/token')
-              .set('X-Authub-Account', masterAccount.name)
-              .send({
-                  username: testUser1.username,
-                  password: 'Abc789',
-                  grant_type: 'password'
-              })
-              .expect(200)
-          })
-          .then( res => {
-            done();
-          })
-          .catch( err => {
-            done(err)
-          })
-      })
-    })
+    // it('Change password with forgot password token', (done) => {
+    //   dbs.connectToMaster((err, db) => {
+    //     db.model('ForgotPasswordRequest')
+    //       .findOne({ identity: testUser1.email, used: false })
+    //       .sort({ createdAt: 'desc'})
+    //       .then( forgotPasswordRequest => {
+    //         return request(app)
+    //           .post('/identity/users/forgot_password')
+    //           .set('X-Authub-Account', masterAccount.name)
+    //           .send({
+    //             email: forgotPasswordRequest.identity,
+    //             token: forgotPasswordRequest.token,
+    //             password: 'Abc789'
+    //           })
+    //           .expect(200)
+    //       })
+    //       .then( res => {
+    //         return request(app)
+    //           .post('/identity/oauth2/token')
+    //           .set('X-Authub-Account', masterAccount.name)
+    //           .send({
+    //               username: testUser1.username,
+    //               password: 'Abc789',
+    //               grant_type: 'password'
+    //           })
+    //           .expect(200)
+    //       })
+    //       .then( res => {
+    //         done();
+    //       })
+    //       .catch( err => {
+    //         done(err)
+    //       })
+    //   })
+    // })
   })
 })
